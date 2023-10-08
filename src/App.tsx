be95cf0ae7836bacce0a04b65f1f48d877362da8
Artur <alimayko91@gmail.com>
2023-10-08 22:29:45 +0300

    complete newTask function

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import TodoList, {TaskType} from "./TodoList";
+import {v1} from "uuid";
 
 
 export type FilterValuesType = "all" | "completed" | "active";
@@ -14,20 +15,35 @@ function App() {
 
     //удаление задач
     let [tasks, setTasks] = useState<Array<TaskType>>([
-            {id: 1, isDone: true, title: "HTML&CSS"},
-            {id: 2, isDone: true, title: "JS"},
-            {id: 3, isDone: false, title: "React"},
-            {id: 4, isDone: false, title: "Redux"},
+            {id: v1(), isDone: true, title: "HTML&CSS"},
+            {id: v1(), isDone: true, title: "JS"},
+            {id: v1(), isDone: false, title: "React"},
+            {id: v1(), isDone: false, title: "Redux"},
         ]
     );
+
+    console.log(tasks)
 
     //фильтрация задач
     let [filter, setFilter] = useState<FilterValuesType>("all");
 
     //функция удаления задач
-    function removeTask(id: number) {
+    function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id !== id)
+        //вызываем функцию setTasks во время удаления задачи и засовываем в него отфильтрованую таску которую удалили
         setTasks(filteredTasks);
+    }
+
+
+
+    function addTask (title: string) {//функция добавления новой таски
+        let newTask = {id: v1(),
+            title: title,
+            isDone: false
+        };//сгенерируем новый объект такси у нее есть id title isDone
+        let newTasks = [ newTask, ...tasks]; //если мы хотим получить новый массив в котором добавилась task, то мы должны создать новый массив newTasks и сказать добавь новую newTasks в начало массива, а затем добавь те таски которые сидели в старом массиве tasks , будет выглядеть так let newTasks = [ newTask, ...tasks];
+        //первым элементом положили новый объект а последним элементом - достали все объекты из старого массива ...tasks];
+        setTasks(newTasks); // - тут засовываем новый массив с добавленной  новой newTask
     }
 
     let tasksForTodolist = tasks;
@@ -55,6 +71,7 @@ function App() {
                 title={todoListTitle_1}
                 removeTask={removeTask}
                 changeFilter={changeFilter}
+                addTask={addTask}//addTask без скобок!иначе будет зацикливание - бесконечный цикл
             />
         </div>
     );
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {FilterValuesType} from "./App";
 
 
@@ -6,30 +6,47 @@ type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
     // tasks: TaskType[] такой вариант тоже верный
-    removeTask: (id: number) => void
+    // функция removeTask  - функция которая ждет что ей дадут функцию которая принимает строку
+    removeTask: (taskId: string) => void
     changeFilter:(value:FilterValuesType) => void
+    addTask: (title: string) => void // функция которая не принимает ничего и не возвращает ничего
 }
 
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
 
 
 //в props передаем данные . которые в свою очередь управляются PropsType которые описаны выше
-const TodoList: FC<TodoListPropsType> = ({title, tasks,removeTask,changeFilter}) => {
+const TodoList: FC<TodoListPropsType> = ({title, tasks,removeTask,changeFilter,addTask}) => {
+
+    const [newTaskTitle, setNewTaskTitle] = useState("")
 
     return (
         <div className="todolist">
             <h3>{title}</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input value={newTaskTitle} onChange={(event)=> {
+                    // при попытке изменить input срабатывает событие onChange
+                    // внутри onChange вызываем функцию и сетаем новое значение с помощью функции setNewTaskTitle
+                    // и значение которое в момент onChange сидит в инпуте, мы можем получить с помощью
+                    // объекта события event или e . в нем нас интересует event.currentTarget то есть это элемент с которым произошло событие
+                    // обращаемся к event.currentTarget и берем его текущее значение event.currentTarget.value
+                    //и мы это value отправляем в локальный state - setNewTaskTitle
+                    setNewTaskTitle(event.currentTarget.value)}}/>
+                {/*по нажатию добавляет новую task*/}
+                <button onClick={ () => {
+                    addTask(newTaskTitle)
+                    //очищаем значение в state вызываем  setNewTaskTitle("") и добавляем пустую строку
+                    setNewTaskTitle("")
+                }}>+</button>
             </div>
             <ul>
                 {
-                    tasks.map( t => <li><input type="checkbox" checked={t.isDone}/>
+                    tasks.map( t => <li key={t.id}>
+                        <input type="checkbox" checked={t.isDone}/>
                         <span>{t.title}</span>
                         <button onClick={ () => { removeTask(t.id)}  }>x</button>
                     </li>)
