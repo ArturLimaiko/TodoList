de1b323fb11fc4807ce757455f5f79cdbae06939
Artur <alimayko91@gmail.com>
2023-10-09 14:00:42 +0300

    complete add task addTaskHandler(функция добавления новой задачи)

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useState} from 'react';
+import React, {ChangeEvent, FC, KeyboardEvent, MouseEventHandler, useState} from 'react';
 import {FilterValuesType} from "./App";
 
 
@@ -20,38 +20,43 @@ export type TaskType = {
 
 
 //в props передаем данные . которые в свою очередь управляются PropsType которые описаны выше
-const TodoList: FC<TodoListPropsType> = ({title, tasks,removeTask,changeFilter,addTask}) => {
-
+const TodoList: FC<TodoListPropsType> = ({title, tasks,removeTask,addTask,changeFilter}) => {
     const [newTaskTitle, setNewTaskTitle] = useState("")
+
+//тут храним анонимные функции ,которые мы передаем на различные события
+    //функция для обработчика onChange, в параметрах указали - event : ChangeEvent<HTMLInputElement>
+    const onNewTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setNewTaskTitle(event.currentTarget.value)
+    }
+
+    //функция для обработчика onKeyPress, в параметрах указали - event: KeyboardEvent<HTMLInputElement>
+    const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
+            if (event.charCode === 13) {//если нажали  клавишу enter то таска добавится
+                addTask(newTaskTitle) //добавляется таска
+                setNewTaskTitle("") //после как таска добавилась строка ввода очиститься
+            }
+    }
+
+    //функция добавления новой задачи
+    const addTaskHandler = () => {
+        addTask(newTaskTitle)
+        //очищаем значение в state вызываем  setNewTaskTitle("") и добавляем пустую строку
+        setNewTaskTitle("")
+    }
 
     return (
         <div className="todolist">
             <h3>{title}</h3>
             <div>
                 <input value={newTaskTitle}
-                       onChange={(event)=> {
-                    // при попытке изменить input срабатывает событие onChange
-                    // внутри onChange вызываем функцию и сетаем новое значение с помощью функции setNewTaskTitle
-                    // и значение которое в момент onChange сидит в инпуте, мы можем получить с помощью
-                    // объекта события event или e . в нем нас интересует event.currentTarget то есть это элемент с которым произошло событие
-                    // обращаемся к event.currentTarget и берем его текущее значение event.currentTarget.value
-                    //и мы это value отправляем в локальный state - setNewTaskTitle
-                    setNewTaskTitle(event.currentTarget.value)}}
+                       //инпут сам вызовет функцию onNewTitleChangeHandler ,когда произойдет событие onChange
+                       onChange={onNewTitleChangeHandler}
 
                        //когда произойдет нажатие клавиши вызови нашу функцию ,сюда придет объект event
-                    onKeyPress={(event) => {
-                        if (event.charCode === 13) {//если нажали  клавишу enter то таска добавится
-                            addTask(newTaskTitle)
-                            setNewTaskTitle("")
-                        }
-                    }}
+                    onKeyPress={onKeyPressHandler}
                 />
                 {/*по нажатию добавляет новую task*/}
-                <button onClick={ () => {
-                    addTask(newTaskTitle)
-                    //очищаем значение в state вызываем  setNewTaskTitle("") и добавляем пустую строку
-                    setNewTaskTitle("")
-                }}>+</button>
+                <button onClick={addTaskHandler}>+</button>
             </div>
             <ul>
                 {
