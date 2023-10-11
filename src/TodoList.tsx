8de4c9a45c996a7da5890cff5d582ee95909ea40
Artur <alimayko91@gmail.com>
2023-10-11 16:38:20 +0300

    complete onRemoveHandler(функция удаления для каждой новой кнопки)

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -8,7 +8,7 @@ type TodoListPropsType = {
     // tasks: TaskType[] такой вариант тоже верный
     // функция removeTask  - функция которая ждет что ей дадут функцию которая принимает строку
     removeTask: (taskId: string) => void
-    changeFilter:(value:FilterValuesType) => void
+    changeFilter: (value: FilterValuesType) => void
     addTask: (title: string) => void // функция которая не принимает ничего и не возвращает ничего
 }
 
@@ -20,10 +20,11 @@ export type TaskType = {
 
 
 //в props передаем данные . которые в свою очередь управляются PropsType которые описаны выше
-const TodoList: FC<TodoListPropsType> = ({title, tasks,removeTask,addTask,changeFilter}) => {
+const TodoList: FC<TodoListPropsType> = ({title, tasks, removeTask, addTask, changeFilter}) => {
     const [newTaskTitle, setNewTaskTitle] = useState("")
 
-//тут храним анонимные функции ,которые мы передаем на различные события
+
+//ТУТ ХРАНИМ анонимные функции ,которые мы передаем на различные события
     //функция для обработчика onChange, в параметрах указали - event : ChangeEvent<HTMLInputElement>
     const onNewTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         setNewTaskTitle(event.currentTarget.value)
@@ -31,10 +32,10 @@ const TodoList: FC<TodoListPropsType> = ({title, tasks,removeTask,addTask,change
 
     //функция для обработчика onKeyPress, в параметрах указали - event: KeyboardEvent<HTMLInputElement>
     const onKeyPressHandler = (event: KeyboardEvent<HTMLInputElement>) => {
-            if (event.charCode === 13) {//если нажали  клавишу enter то таска добавится
-                addTask(newTaskTitle) //добавляется таска
-                setNewTaskTitle("") //после как таска добавилась строка ввода очиститься
-            }
+        if (event.charCode === 13) {//если нажали  клавишу enter то таска добавится
+            addTask(newTaskTitle) //добавляется таска
+            setNewTaskTitle("") //после как таска добавилась строка ввода очиститься
+        }
     }
 
     //функция добавления новой задачи
@@ -44,33 +45,51 @@ const TodoList: FC<TodoListPropsType> = ({title, tasks,removeTask,addTask,change
         setNewTaskTitle("")
     }
 
+
+    //ФУНКЦИИ С ФИЛЬТРАМИ
+    const onAllClickHandler = () => changeFilter("all");
+    const onActiveClickHandler = () => changeFilter("active");
+    const onCompletedClickHandler = () => changeFilter("completed");
+
+
     return (
         <div className="todolist">
             <h3>{title}</h3>
             <div>
                 <input value={newTaskTitle}
-                       //инпут сам вызовет функцию onNewTitleChangeHandler ,когда произойдет событие onChange
+                    //инпут сам вызовет функцию onNewTitleChangeHandler ,когда произойдет событие onChange
                        onChange={onNewTitleChangeHandler}
 
-                       //когда произойдет нажатие клавиши вызови нашу функцию ,сюда придет объект event
-                    onKeyPress={onKeyPressHandler}
+                    //когда произойдет нажатие клавиши вызови нашу функцию onKeyPressHandler,сюда придет объект event
+                       onKeyPress={onKeyPressHandler}
                 />
                 {/*по нажатию добавляет новую task*/}
                 <button onClick={addTaskHandler}>+</button>
             </div>
             <ul>
+                {/*проходимся map вызывает стрелочную функцию внутри мы создаем новую функцию колбек
+                которой передаем каждую кнопку х по этому у каждой кнопки x свой колбек */}
                 {
-                    tasks.map( t => <li key={t.id}>
-                        <input type="checkbox" checked={t.isDone}/>
-                        <span>{t.title}</span>
-                        <button onClick={ () => { removeTask(t.id)}  }>x</button>
-                    </li>)
+                    tasks.map(t => {
+                        //объявляем функцию onRemoveHandler . каждый раз onRemoveHandler обращается к своей таске
+                        //через свой id и этот onRemoveHandler отдаем каждый раз новой созданной кнопке
+                        const onRemoveHandler = () => {
+                            removeTask(t.id)
+                        }
+                        return <li key={t.id}>
+                            <input type="checkbox" checked={t.isDone}/>
+                            <span>{t.title}</span>
+                            {/*Функция удаления не общая. мы не можем вынести ее наверх.она своя у каждой <li> */}
+                            <button onClick={onRemoveHandler}>x</button>
+                        </li>
+                    })
                 }
             </ul>
             <div>
-                <button onClick={ () => { changeFilter("all")}} >All</button>
-                <button onClick={ () => { changeFilter("active")}} >Active</button>
-                <button onClick={ () => { changeFilter("completed")}} >Completed</button>
+                {/*//передаем функции в кнопки. при клике говорим что вызывай функцию обработчик.onAllClickHandler onActiveClickHandler onCompletedClickHandler*/}
+                <button onClick={onAllClickHandler}>All</button>
+                <button onClick={onActiveClickHandler}>Active</button>
+                <button onClick={onCompletedClickHandler}>Completed</button>
             </div>
         </div>
     )
